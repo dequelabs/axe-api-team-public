f6dc30017014138c60d1c6306ab1e81aef0c63d2
Gabe <41127686+Zidious@users.noreply.github.com>
2023-09-18 20:53:58 +0100

    lets just creating an issue

--- .github/actions/create-project-issue-v1/dist/index.js
+++ .github/actions/create-project-issue-v1/dist/index.js
@@ -9691,39 +9691,18 @@ async function run(core, github) {
             core.setFailed('project_id must be a number');
             return;
         }
-        const owner = github.context.repo.owner;
-        const repo = repository || github.context.repo.repo;
         const octokit = github.getOctokit(token);
-        const [{ data: issueCreated }, { data: project }] = await Promise.all([
-            octokit.rest.issues.create({
-                owner,
-                repo,
-                title,
-                body,
-                labels: labels ? labels.split(',') : undefined,
-                assignees: assignees ? assignees.split(',') : undefined
-            }),
-            octokit.rest.projects.get({
-                project_id: projectId
-            })
-        ]);
+        const { data: issueCreated } = await octokit.rest.issues.create({
+            owner: github.context.repo.owner,
+            repo: repository || github.context.repo.repo,
+            title,
+            body,
+            labels: labels ? labels.split(',') : undefined,
+            assignees: assignees ? assignees.split(',') : undefined
+        });
         core.info(`${issueCreated}`);
-        core.info(`${project}`);
         core.info(`Created issue ${issueCreated.number}`);
         core.info(`Adding issue ${issueCreated.number} to project ID ${projectId}`);
-        const { data: projectColumns } = await octokit.rest.projects.listColumns({
-            project_id: project.id
-        });
-        let projectColumn = projectColumns.find(column => column.name.toLowerCase() === columnName.toLowerCase());
-        if (!projectColumn) {
-            core.warning(`Column ${columnName} not found, defaulting to Backlog column`);
-            projectColumn = projectColumns.find(column => column.name.toLowerCase() === 'backlog');
-        }
-        await octokit.rest.projects.createCard({
-            column_id: projectColumn.id,
-            content_id: issueCreated.id,
-            content_type: 'Issue'
-        });
         core.setOutput('issue_url', issueCreated.url);
     }
     catch (error) {
