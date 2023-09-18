0e66e48d49b541bb3cd7281eea41c213ea670e04
Gabe <41127686+Zidious@users.noreply.github.com>
2023-09-18 19:59:22 +0100

    build dist

---
+++ .github/actions/create-project-issue-v1/dist/types.d.ts
@@ -0,0 +1,4 @@
+import core from '@actions/core';
+import github from '@actions/github';
+export declare type Core = Pick<typeof core, 'getInput' | 'setFailed' | 'info' | 'warning' | 'setOutput'>;
+export declare type GitHub = Pick<typeof github, 'getOctokit' | 'context'>;
