0c91f14b548684cc7b7180915472ca35ee006a26
Gabe <41127686+Zidious@users.noreply.github.com>
2023-09-20 17:48:47 +0100

    move graphql queries inline so when refactoring it will  be easier

--- .github/actions/create-project-issue-v1/src/addToBoard.ts
+++ .github/actions/create-project-issue-v1/src/addToBoard.ts
@@ -1,12 +1,68 @@
 import { getOctokit } from '@actions/github'
-import {
-  ADD_ISSUE_TO_PROJECT_BOARD,
-  GET_PROJECT_BOARD_BY_NUMBER,
-  MOVE_CARD_TO_COLUMN
-} from './constants'
 import type { AddProjectCardResponse, ProjectBoardResponse } from './types'
 
 // TODO: Refactor .github/actions/add-to-board to use this
+
+import dedent from 'dedent'
+
+// @see //@see https://docs.github.com/en/issues/planning-and-tracking-with-projects/automating-your-project/using-the-api-to-manage-projects#finding-information-about-projects
+export const GET_PROJECT_BOARD_BY_NUMBER = dedent`
+  query($owner: String!, $projectNumber: Int!) {
+    organization(login: $owner) {
+      projectV2(number: $projectNumber) {
+        id
+        fields(first:20) {
+          nodes {
+            ... on ProjectV2Field {
+              id
+              name
+            }
+            ... on ProjectV2SingleSelectField {
+              id
+              name
+              options {
+                id
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+//@see https://docs.github.com/en/graphql/reference/mutations#updateissue
+export const ADD_ISSUE_TO_PROJECT_BOARD = dedent`
+  mutation (
+    $projectId: ID!
+    $issueId: ID!
+  ){
+    addProjectV2ItemById(input: {projectId: $projectId contentId:$issueId}) {
+      item {
+        id
+      }
+    }
+  }
+`
+
+//@see https://docs.github.com/en/graphql/reference/mutations#updateprojectv2itemfieldvalue
+export const MOVE_CARD_TO_COLUMN = dedent`
+  mutation (
+    $projectId: ID!
+    $itemId: ID!
+    $fieldId: ID!
+    $value: String!
+  ){
+    updateProjectV2ItemFieldValue(input: {projectId: $projectId itemId: $itemId fieldId: $fieldId value: {
+      singleSelectOptionId: $value
+    }}) {
+    projectV2Item {
+        id
+    }
+    }
+  }
+`
 
 interface AddToBoardArgs {
   octokit: ReturnType<typeof getOctokit>
