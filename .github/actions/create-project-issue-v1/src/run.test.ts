8444e028578f4f562b42bd188edefed884968c18
Gabe <41127686+Zidious@users.noreply.github.com>
2023-09-26 16:01:36 +0100

    use `withArgs` instead of `call(..)`, and add more tests

--- .github/actions/create-project-issue-v1/src/run.test.ts
+++ .github/actions/create-project-issue-v1/src/run.test.ts
@@ -28,7 +28,7 @@ describe('run', () => {
           create: sinon.stub().resolves({
             data: {
               html_url: 'https://deque.bizzy.com',
-              node_id: 'generated-node-id'
+              node_id: 'issue-id'
             }
           })
         }
@@ -46,30 +46,59 @@ describe('run', () => {
 
   afterEach(sinon.restore)
 
-  const createAndMoveIssueStub = async () => {
+  interface CreateAndMoveIssueStubArgs {
+    projectNumber?: string
+    owner?: string
+  }
+
+  const createAndMoveIssueStub = async (
+    options?: Partial<CreateAndMoveIssueStubArgs>
+  ) => {
     //`getProjectBoardID` stub
-    getExecOutput.onFirstCall().resolves({
-      stdout: JSON.stringify(MOCK_PROJECT_BOARD_ID),
-      exitCode: 0
-    })
+    getExecOutput
+      .withArgs(
+        `gh project view ${options?.projectNumber ?? 66} --owner ${
+          options?.owner ?? 'owner'
+        } --format json`
+      )
+      .resolves({
+        stdout: JSON.stringify(MOCK_PROJECT_BOARD_ID),
+        exitCode: 0
+      })
 
     //`addIssueToBoard` stub
-    getExecOutput.onSecondCall().resolves({
-      stdout: JSON.stringify(MOCK_ISSUE_ADDED),
-      exitCode: 0
-    })
+    getExecOutput
+      .withArgs(
+        `gh project item-add ${options?.projectNumber ?? '66'} --owner ${
+          options?.owner ?? 'owner'
+        } --url https://deque.bizzy.com --format json`
+      )
+      .resolves({
+        stdout: JSON.stringify(MOCK_ISSUE_ADDED),
+        exitCode: 0
+      })
 
     //`getProjectFieldList` stub
-    getExecOutput.onThirdCall().resolves({
-      stdout: JSON.stringify(MOCK_FIELD_LIST),
-      exitCode: 0
-    })
+    getExecOutput
+      .withArgs(
+        `gh project field-list ${options?.projectNumber ?? '66'} --owner ${
+          options?.owner ?? 'owner'
+        } --format json`
+      )
+      .resolves({
+        stdout: JSON.stringify(MOCK_FIELD_LIST),
+        exitCode: 0
+      })
 
     //`moveIssueToColumn` stub
-    getExecOutput.onCall(3).resolves({
-      stdout: JSON.stringify(MOCK_ISSUE_MOVED),
-      exitCode: 0
-    })
+    getExecOutput
+      .withArgs(
+        'gh project item-edit --id 123 --field-id 123 --single-select-option-id 456 --project-id 123 --format json'
+      )
+      .resolves({
+        stdout: JSON.stringify(MOCK_ISSUE_MOVED),
+        exitCode: 0
+      })
   }
 
   interface GenerateInputs {
@@ -291,6 +320,70 @@ describe('run', () => {
 
       assert.isTrue(setFailed.calledOnce)
       assert.isTrue(setFailed.calledWith('Column foobar not found'))
+    })
+  })
+
+  describe('given no `project_number` input', () => {
+    it('defaults to 66', async () => {
+      generateInputs()
+
+      await createAndMoveIssueStub()
+
+      const core = {
+        getInput,
+        setFailed,
+        setOutput,
+        info
+      }
+
+      await run(core as unknown as Core, github as unknown as GitHub)
+
+      // commands that require the project number
+      const [projectBoarArgs, issueAddedArgs, fieldListArgs] =
+        getExecOutput.args
+
+      assert.include(projectBoarArgs[0], '66')
+      assert.include(issueAddedArgs[0], '66')
+      assert.include(fieldListArgs[0], '66')
+
+      assert.isTrue(setFailed.notCalled)
+      assert.equal(info.callCount, 6)
+      assert.isTrue(setOutput.calledOnce)
+      assert.isTrue(
+        setOutput.calledWith('issue_url', 'https://deque.bizzy.com')
+      )
+    })
+  })
+
+  describe('given a `project_number` input', () => {
+    it('uses the `project_number` input', async () => {
+      generateInputs({ project_number: '99' })
+
+      await createAndMoveIssueStub({ projectNumber: '99' })
+
+      const core = {
+        getInput,
+        setFailed,
+        setOutput,
+        info
+      }
+
+      await run(core as unknown as Core, github as unknown as GitHub)
+
+      // commands that require the project number
+      const [projectBoarArgs, issueAddedArgs, fieldListArgs] =
+        getExecOutput.args
+
+      assert.include(projectBoarArgs[0], '99')
+      assert.include(issueAddedArgs[0], '99')
+      assert.include(fieldListArgs[0], '99')
+
+      assert.isTrue(setFailed.notCalled)
+      assert.equal(info.callCount, 6)
+      assert.isTrue(setOutput.calledOnce)
+      assert.isTrue(
+        setOutput.calledWith('issue_url', 'https://deque.bizzy.com')
+      )
     })
   })
 })
