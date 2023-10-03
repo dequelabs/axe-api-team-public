d638b545a77b0568988c8f8e5b64a77d7a788554
Gabe <41127686+Zidious@users.noreply.github.com>
2023-10-03 16:22:09 +0100

    build(./github/actions): remove `dist` on rebuild within `label-and-move-released-issues-v1`

--- .github/actions/label-and-move-released-issues-v1/dist/getProjectBoardID.d.ts
+++
@@ -1,9 +0,0 @@
-interface GetProjectBoardIDResponse {
-    id: string;
-}
-interface GetProjectBoardIDArgs {
-    projectNumber: number;
-    owner: string;
-}
-export default function getProjectBoardID({ projectNumber, owner }: GetProjectBoardIDArgs): Promise<GetProjectBoardIDResponse>;
-export {};
