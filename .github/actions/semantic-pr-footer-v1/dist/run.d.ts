15e95cdb81b65c963e34d7c6aa5edf8bfc2ba1a3
Steven Lambert <steven.lambert@deque.com>
2023-10-05 11:25:55 -0600

    chore: run build

--- .github/actions/semantic-pr-footer-v1/dist/run.d.ts
+++ .github/actions/semantic-pr-footer-v1/dist/run.d.ts
@@ -1,4 +1,4 @@
 import type core from '@actions/core';
 import type github from '@actions/github';
-export type Core = Pick<typeof core, 'setFailed' | 'info'>;
+export declare type Core = Pick<typeof core, 'setFailed' | 'info'>;
 export default function run(core: Core, payload?: typeof github.context.payload): void;
