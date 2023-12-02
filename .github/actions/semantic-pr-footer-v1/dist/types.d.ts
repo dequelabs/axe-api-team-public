8abfe96e6f40adaa3fcafacfa67d4fdd5fe98725
Gabe <41127686+Zidious@users.noreply.github.com>
2023-12-02 18:08:15 +0000

    fix(.github/actions/semanetic-pr-footer-v1): add a list of known gitub actors to skip validation (#99)

---
+++ .github/actions/semantic-pr-footer-v1/dist/types.d.ts
@@ -0,0 +1,4 @@
+import github from '@actions/github';
+import core from '@actions/core';
+export type Core = Pick<typeof core, 'setFailed' | 'info' | 'getInput'>;
+export type GitHub = Pick<typeof github, 'context'>;
