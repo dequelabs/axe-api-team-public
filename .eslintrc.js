7555b0a12cfa9fe75d9c4bfaf6b04e4d2308b2e9
Gabe <41127686+Zidious@users.noreply.github.com>
2023-12-07 20:33:26 +0000

    chore: add mocha eslint plugin and block `only` tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,17 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:mocha/recommended'
   ],
   rules: {
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/no-use-before-define': 'off'
+    '@typescript-eslint/no-use-before-define': 'off',
+    'mocha/no-mocha-arrows': 'off',
+    'mocha/no-setup-in-describe': 'off',
+    // We export test functions / utilities to be used in other tests within the same repo
+    'mocha/no-exports': 'off',
+    'mocha/no-exclusive-tests': 'error'
   },
   env: {
     node: true,
