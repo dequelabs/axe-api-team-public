4973b0f665909a4de06678dd65a101f7174da4ff
Gabe <41127686+Zidious@users.noreply.github.com>
2023-12-13 15:55:44 +0000

    feat(.github/actions): add `get-release-issue-v1` action (#101)
    Co-authored-by: Steven Lambert <2433219+straker@users.noreply.github.com>

---
+++ .github/actions/get-release-issue-v1/src/index.ts
@@ -0,0 +1,5 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import run from './run'
+
+run(core, github)
---
+++ .github/actions/get-release-issue-v1/src/run.ts
@@ -0,0 +1,47 @@
+import { getExecOutput } from '@actions/exec'
+import type { GitHub, Core, Issues } from './types'
+
+export default async function run(core: Core, github: GitHub) {
+  try {
+    const version = core.getInput('version', { required: true }).trim()
+    const { owner, repo } = github.context.repo
+    const ownerInput = (core.getInput('owner') || owner).toLowerCase().trim()
+    const repoInput = (core.getInput('repo') || repo).toLowerCase().trim()
+
+    core.info(`Getting issues for ${ownerInput}/${repoInput}...`)
+
+    const {
+      stdout: issueList,
+      stderr: issueListError,
+      exitCode: issuesListExitCode
+    } = await getExecOutput(
+      `gh issue list --repo ${ownerInput}/${repoInput} --label release --state open --json url,title --search "${owner}/${repo} v${version}"`
+    )
+
+    if (issuesListExitCode) {
+      throw new Error(`Error getting issues: \n${issueListError}`)
+    }
+
+    const issues: Issues = JSON.parse(issueList)
+
+    if (!issues.length) {
+      core.warning(
+        `No issues found for ${owner}/${repo} v${version}. It may have already been closed...`
+      )
+      return
+    }
+
+    if (issues.length > 1) {
+      throw new Error(
+        `Found ${issues.length} issues for ${owner}/${repo} v${version}. Please manually verify...`
+      )
+    }
+
+    const issue = issues[0]
+
+    core.info(`Found issue: ${issue.url}. Setting "issue-url" output...`)
+    core.setOutput('issue-url', issue.url)
+  } catch (error) {
+    core.setFailed((error as Error).message)
+  }
+}
