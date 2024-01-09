9ada684427e50baa5fe94789c4943d7ee42ca4e2
Steven Lambert <2433219+straker@users.noreply.github.com>
2024-01-09 12:10:01 -0700

    fix(semantic-pr-footer-v1): ignore empty lines at end of body (#119)

--- .github/actions/label-and-move-released-issues-v1/dist/generate-commit-list-v1/src/types.d.ts
+++ .github/actions/label-and-move-released-issues-v1/dist/generate-commit-list-v1/src/types.d.ts
@@ -1,16 +1,16 @@
 import type core from '@actions/core';
 import type github from '@actions/github';
-export type Core = Pick<typeof core, 'getInput' | 'setOutput' | 'info' | 'setFailed'>;
-export type Github = Pick<typeof github, 'context'>;
-export type GetRawCommitListParams = {
+export declare type Core = Pick<typeof core, 'getInput' | 'setOutput' | 'info' | 'setFailed'>;
+export declare type Github = Pick<typeof github, 'context'>;
+export declare type GetRawCommitListParams = {
     base: string;
     head: string;
 };
-export type GetParsedCommitListParams = {
+export declare type GetParsedCommitListParams = {
     rawCommitList: string[];
     repository: string;
 };
-export type ParsedCommitList = {
+export declare type ParsedCommitList = {
     commit: string;
     title: string;
     sha: string;
