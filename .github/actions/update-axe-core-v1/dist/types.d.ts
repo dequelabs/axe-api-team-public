626d01d42d7e2924bd5af769d5fe99066c31cec0
Gabe <41127686+Zidious@users.noreply.github.com>
2024-01-09 20:54:28 +0000

    leave input default as empty string

--- .github/actions/update-axe-core-v1/dist/types.d.ts
+++ .github/actions/update-axe-core-v1/dist/types.d.ts
@@ -1,3 +1,3 @@
 import type core from '@actions/core';
-export declare type Core = Pick<typeof core, 'setOutput' | 'info' | 'setFailed'>;
-export declare type PackageManager = Promise<'npm' | 'yarn' | undefined>;
+export type Core = Pick<typeof core, 'setOutput' | 'info' | 'setFailed'>;
+export type PackageManager = Promise<'npm' | 'yarn' | undefined>;
