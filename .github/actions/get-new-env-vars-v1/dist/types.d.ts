a6a6c67589d5a92ea11c45b5c03cc852b6e33c3c
Gabe <41127686+Zidious@users.noreply.github.com>
2024-01-11 17:00:03 +0000

    feat(.github/actions): add `abort-release-candidate-v1` action (#102)

--- .github/actions/get-new-env-vars-v1/dist/types.d.ts
+++ .github/actions/get-new-env-vars-v1/dist/types.d.ts
@@ -1,2 +1,2 @@
 import type core from '@actions/core';
-export declare type Core = Pick<typeof core, 'getInput' | 'setOutput' | 'info' | 'setFailed'>;
+export type Core = Pick<typeof core, 'getInput' | 'setOutput' | 'info' | 'setFailed'>;
--- .github/actions/is-release-week-v1/dist/run.d.ts
+++ .github/actions/is-release-week-v1/dist/run.d.ts
@@ -1,3 +1,3 @@
 import type core from '@actions/core';
-export declare type Core = Pick<typeof core, 'getInput' | 'info' | 'setFailed' | 'setOutput'>;
+export type Core = Pick<typeof core, 'getInput' | 'info' | 'setFailed' | 'setOutput'>;
 export default function run(core: Core): void;
