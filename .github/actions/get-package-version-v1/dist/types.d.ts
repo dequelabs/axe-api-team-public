9964554735565039e9b854cc364a852f1eb27867
Jonathan Garbee <jonathan.garbee@deque.com>
2026-06-16 12:17:38 -0400

    build(get-package-version-v1): migrate to ESM + node:test

--- .github/actions/get-package-version-v1/dist/types.d.ts
+++
@@ -1,8 +0,0 @@
-import type core from '@actions/core';
-import type { readFileSync } from 'fs';
-export type Core = Pick<typeof core, 'getInput' | 'setOutput' | 'info' | 'setFailed' | 'warning'>;
-export type FileData = {
-    version: string;
-    [key: string]: unknown;
-};
-export type readFileFS = typeof readFileSync;
